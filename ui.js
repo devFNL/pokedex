071d16d2640964c4efeb92971853d1e0dd514459
Federico Lindberg <fedelindberg@gmail.com>
2023-08-02 17:16:42 -0300

    v1.2 (redesign)
    added new feature that changes card background color to match the pokemon type.
    redesign of some elements.

--- ui.js
+++ ui.js
@@ -11,6 +11,37 @@ const $listContainer = document.querySelector("#listCont");
 const $nextButton = document.querySelector("#next");
 const $previousButton = document.querySelector("#previous");
 
+const typeColors = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
+
+function setCardBackgroundColor(type) {
+  const card = document.querySelector(".card");
+  const color = typeColors[type];
+  if (color) {
+    card.style.backgroundColor = color;
+  } else {
+    card.style.backgroundColor = "white";
+  }
+}
+
 function printPokemonDetails(pokemon) {
   $name.innerText = pokemon.name.toUpperCase();
   $pic.src = pokemon.sprites.front_default;
@@ -26,6 +57,8 @@ function printPokemonDetails(pokemon) {
   $type.innerText = "TYPE: " + tipos;
   $height.innerText = "Height: " + pokemon.height + " ft.";
   $weight.innerText = "Weight: " + pokemon.weight + " lbs.";
+
+  setCardBackgroundColor(pokemon.types[0].type.name.toLowerCase());
 }
 
 function mapPokemonToButton(pokemon) {
